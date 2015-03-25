29555c9d62fe917fa0a38c6d775bf168f51d2501
KateCoding <kathylynne.johnson@gmail.com>
2015-03-24 22:07:03 -0300

    Removed: Recipe.java, Ingredient.java - LayoutOperation.java should be removed next.
    AddRecipeActivity creates recipe properly, also populates Ingredients table.
    Modified activity_add_recipe to properly reflect additional fields, and minor visual improvement.
    Added step_rowdetail xml to handle step view
    
    LoginActivity Modified to Toast user Id for testing.
    
    RegisterActivity has logic added for login, but is not implemented where we need it.  (used for testing - works)

--- app/src/main/java/com/kathylynne/chowsense/app/AddRecipeActivity.java
+++ app/src/main/java/com/kathylynne/chowsense/app/AddRecipeActivity.java
@@ -1,18 +1,15 @@
 package com.kathylynne.chowsense.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
-import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.SaveCallback;
+import android.view.ViewGroup;
+import android.widget.*;
+import com.parse.*;
 
 import java.util.ArrayList;
 
@@ -20,46 +17,54 @@ import java.util.ArrayList;
 public class AddRecipeActivity extends ActionBarActivity {
 
     ImageButton btnAdd;
-    Button btnDisplay;
+    ImageButton btnAddStep;
+
     EditText title;
     EditText description;
-    ArrayList<String> ingredientIds = new ArrayList<String>();
     ArrayList<String> steps = new ArrayList<String>();
-    String recipeId;
+    public String recipeId;
     Button btnSave;
-    // Recipe recipe = new Recipe();
-    ParseObject recipe = new ParseObject("Recipe");
+    final ParseObject recipe = new ParseObject("Recipe");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
-        //initialize fields
-        //ParseObject.registerSubclass(Recipe.class);
-        Parse.initialize(this, "qJwvg8qtJEb7FnzU1ygRwgdUkGp7Bgh2oV8m2yWP", "TTfQmmrAbfBFu9IGxOQb6oeSvEWLo8TliM6kgj8a");
 
+        //initialize fields
+        //TODO these keys need to be verified
+        Parse.initialize(this, "qJwvg8qtJEb7FnzU1ygRwgdUkGp7Bgh2oV8m2yWP", "TTfQmmrAbfBFu9IGxOQb6oeSvEWLo8TliM6kgj8a");
         title = (EditText) findViewById(R.id.titleText);
         description = (EditText) findViewById(R.id.descriptionText);
         //for dynamic item add
         btnAdd = (ImageButton) findViewById(R.id.btnAdd);
-        btnDisplay = (Button) findViewById(R.id.btnDisplay);
-        btnSave = (Button) findViewById(R.id.saveRecipeButton);
-        //recipeId = UUID.randomUUID().toString();
+        btnAddStep = (ImageButton) findViewById(R.id.btnAddStep);
 
-        //recipe.setObjectId(recipeId);
+        btnSave = (Button) findViewById(R.id.saveRecipeButton);
+        //Tag for logging
+        final String tag = "tag";
+
         recipe.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
-                recipeId = recipe.getObjectId();
+                if (e == null) {
+                    recipeId = recipe.getObjectId();
+                    //show the recipeId is not null in logCat
+                    Log.i(tag, "object id, " + recipeId);
+                } else {
+                    //didn't work
+                }
             }
         });
 
-        Toast rview = Toast.makeText(this, recipeId, Toast.LENGTH_SHORT);
-        rview.show();
-        LayoutOperation.add(this, btnAdd);
-        LayoutOperation.display(this, btnDisplay, recipeId);
-        //
+        recipeId = recipe.getObjectId();
+        //Toast.makeText(this, recipeId, Toast.LENGTH_SHORT).show();
+
+        //call to make the dynamic ingredient and step buttons inflate
+        add(this, btnAdd);
+        addSteps(this, btnAddStep);
+
 
     }
 
@@ -87,17 +92,101 @@ public class AddRecipeActivity extends ActionBarActivity {
     }
 
     public void saveRecipe(View v) {
-        //JSONArray newIngredients = new JSONArray();
-        //newIngredients.put(ingredientIds);
+
+        //put the steps into the array
+        LinearLayout stepsScrollViewLinearLayout = (LinearLayout) findViewById(R.id.stepLinearLayoutForm);
+        for (int i = 0; i < stepsScrollViewLinearLayout.getChildCount(); i++) {
+            EditText stepField = (EditText) findViewById(R.id.stepText);
+            String stepToSave = stepField.getText().toString();
+            steps.add(stepToSave);
+
+        }
 
 
-        //recipe.setTitle(title.getText().toString());
-        //recipe.setDescription(description.getText().toString());
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Recipe");
 
-        //they see me changing!
+        // Retrieve the object by id
+        query.getInBackground(recipeId, new GetCallback<ParseObject>() {
+            public void done(ParseObject recipe, ParseException e) {
+                if (e == null) {
+                    // Now let's update it with some new data.
+                    recipe.put("RecipeTitle", title.getText().toString());
+                    recipe.put("RecipeDescription", description.getText().toString());
+                    recipe.put("RecipeSteps", steps);
+                    recipe.saveInBackground();
+                }
+            }
+        });
+
+        //save the ingredients that are present at the time of buttonClick
+        LinearLayout scrollViewLinearLayout = (LinearLayout) findViewById(R.id.linearLayoutForm);
+        for (int i = 0; i < scrollViewLinearLayout.getChildCount(); i++) {
+            ParseObject ingredient = new ParseObject("Ingredient");
+            LinearLayout innerLayout = (LinearLayout) scrollViewLinearLayout.getChildAt(i);
+            EditText name = (EditText) innerLayout.findViewById(R.id.editDescricao);
+            EditText qty = (EditText) innerLayout.findViewById(R.id.qtyText);
+            String iName = name.getText().toString();
+            //add steps here
+            String iQty = qty.getText().toString();
+            ingredient.put("IngredientName", iName);
+            //concatenate measure put to have the measure as well.
+            ingredient.put("Measure", iQty);
+            ingredient.put("RecipeId", recipeId);
+            ingredient.saveInBackground();
+        }
+
+
+        Toast.makeText(this, "Recipe Saved!", Toast.LENGTH_SHORT);
+    } // end save method
+
+
+    public void add(final Activity activity, ImageButton btn) {
+        //TODO find a way to turn this into ingredient fields..I believe in us~!
+        final LinearLayout linearLayoutForm = (LinearLayout) activity.findViewById(R.id.linearLayoutForm);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final LinearLayout newView = (LinearLayout) activity.getLayoutInflater().inflate(R.layout.rowdetail, null);
+                newView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                ImageButton btnRemove = (ImageButton) newView.findViewById(R.id.btnRemove);
+
+
+                btnRemove.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        linearLayoutForm.removeView(newView);
+                    }
+                });
+                linearLayoutForm.addView(newView);
+            }
+        });
     }
 
-    public void addIngredientToList(String ingredientId) {
-        ingredientIds.add(ingredientId);
+    public void addSteps(final Activity activity, ImageButton btn) {
+        //TODO find a way to turn this into ingredient fields..I believe in us~!
+        final LinearLayout stepsLinearLayoutForm = (LinearLayout) activity.findViewById(R.id.stepLinearLayoutForm);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final LinearLayout newView = (LinearLayout) activity.getLayoutInflater().inflate(R.layout.step_rowdetail, null);
+                newView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                ImageButton btnRemove = (ImageButton) newView.findViewById(R.id.btnRemove);
+
+
+                btnRemove.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        stepsLinearLayoutForm.removeView(newView);
+                    }
+                });
+                stepsLinearLayoutForm.addView(newView);
+            }
+        });
     }
+
+
 }
